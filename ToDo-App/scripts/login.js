bfa58f3d85c9609c82cfba763ea438c2f4257b2d
Christian Manzaraz <magnusmanz@gmail.com>
2024-03-21 22:40:52 -0300

    Clase 14: Inicio de clase

--- ToDo-App/scripts/login.js
+++ ToDo-App/scripts/login.js
@@ -1,94 +1,94 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-    // const form = document.querySelector("form")
     const form = document.forms[0]
+    // const form = document.querySelector("form")
     const email = document.querySelector("#inputEmail")
     const password = document.querySelector("#inputPassword")
     const url = "https://todo-api.ctd.academy/v1"
 
-    
+
+
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-        event.preventDefault() // prevenimos el comportamiento por defecto del formulairo
+       event.preventDefault()
 
-        // Crear nuestra bomba💣, objeto de lo que capturamos en los inputs para mandarlo en el fetch💥, 
-        const payload = {
-            email: email.value,
-            password: password.value
-        }
-        // console.log(payload);
-        
-        // configuramos nuestro objeto de configuraciones que le pasaremos por parametro al fetch 
+        //Creamos el cuerpo de la request (petición al servidor)
+       const payload = {
+        email: email.value,
+        password: password.value
+      }
+    
+        // vemos el objeto que recibimos del formulario
+    //   console.log(payload);
+
+        //configuramos la request del Fetch
         const settings = {
             method: "POST",
             body: JSON.stringify(payload),
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             }
         }
-        // console.log(settings);
 
-        // Lanzamos la consulta del login a la API
-        realizarLogin(settings)
+    //    console.log(settings);
+
+       realizarLogin(settings)
+
     });
-
-
+    
+    
     /* -------------------------------------------------------------------------- */
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarLogin(settings) {
-    //    console.log(settings);
-       console.log("🎬Lanzar la consulta a la API...");
-
-       fetch(`${url}/users/login`, settings)
-        .then(response => {
-            // console.log(response);
-            if (response.ok) return response.json()
-
-            return Promise.reject(response)
-        })
-        .then(data => {
-            console.log("Promesa cumplida 💍👰🏽‍♀️");
-            console.log(data);
-            console.log(data.jwt);
-
-            if (data.jwt) {
-                // Guardamos el dato jwt en el local storage (este token de autenticación)
-                localStorage.setItem("jwt", JSON.stringify(data.jwt))
-
-                // Limpiar el formulario
-                form.reset()
-                
-                // redireccionamos a nuestro dashboard de la ToDo
-                location.replace("./mis-tareas.html")
-
-            }
-
-
-        })
-        .catch(err => {
-            console.log("Promesa rechazada 🙅🏻‍♀️");
-            console.error(err.status);
-            if (err.status == 400) {
-                console.warn("Contraseña Incorreta");
-                alert("Contraseña Incorreta, por favor revísela");
-            } else if (err.status == 404) {
-                console.warn("El usuario no existe");
-                alert("El usuario no existe, por favor revise su email");
-            } else {
-                console.error("Error de Servidor");
-                alert("Error de Servidor, comúniquese con el proveedor");                
-            }
-        })
-
-
-
-       console.log("🏁Terminamos la consulta a la API...");
+        console.log(settings);
+        console.log("🏄🏻‍♂️Lanzando la consulta a la API....");
         
+        fetch(`${url}/users/login`, settings)
+        .then( response => {
+            console.log(response);
+            // manejar el error de la request, si todo va bien, esta respuesta la capturaremos en el siguiente .then
+            if (response.ok) return response.json()
+            
+            // Si hay un error, fuerzo el error para capturarlo en el .catch
+            return Promise.reject(response)
+            
+            // return response.json().then(errorData => {
+                //     throw new Error(errorData);
+                // })
+                
+            })
+            .then(data =>{
+                console.log(data);
+                console.log(data.jwt);
+                
+                if (data.jwt) {
+                    // Guardamos el dato JWT en LocalStorage (ese token de autenticacion)
+                    localStorage.setItem("jwt", JSON.stringify(data.jwt))
+                    
+                    form.reset() // para limpiar los campos de los inputs del formulario
+                    // redireccionamos a nuestro dashboard de todo
+                    location.replace("./mis-tareas.html")
+                }
+            })
+            .catch( err => {
+                console.error(err);
+                console.error(err.status);
+            if (err.status == 400) {
+                console.warn("Contraseña incorrecta")
+                alert("Contraseña incorrecta. Por favor vuelve a ingresarlo")
+            } else if (err.status == 404) {
+                console.warn("El usuario no existe")
+                alert("El usuario no existe, revise el email")
+            } else {
+                console.error("Error del servidor | url no existe")
+                alert("Error del servidor o url no existe, comúniquese con el proveedor")
+            }
+        })        
     };
 });
 
-// "email": juana@dearco.com
+// "email": "juana@dearco.com",
 // "password": "123456"
